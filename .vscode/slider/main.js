f543f6c1f3bf609d08a50702bafa4ab3a7aa403e
IsmailUmmer <ktismail92@gmail.com>
2025-09-12 01:09:57 +0530

    completed

--- .vscode/slider/main.js
+++ .vscode/slider/main.js
@@ -1,31 +1,33 @@
 const slides = document.querySelectorAll(".images img");
 const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");    
+const nextBtn = document.querySelector(".next");
 let slideIndex = 0;
 let interalId = null;
-console.log(slides)
+
 document.addEventListener("DOMContentLoaded", initiliseSlider);
 
-function initiliseSlider(){
-   slides[slideIndex].classList.add("displayslide");
-   interalId =setInterval(nextslide, 3000);
+function initiliseSlider() {
+    slides[slideIndex].classList.add("displayslide");
+    interalId = setInterval(nextslide, 3000);
 }
-function showslide(index){
-    if(index>= slides.length){
+function showslide(index) {
+    if (index >= slides.length) {
         slideIndex = 0;
     }
-    else if(index < 0) {
-        slideIndex = slides.length-1;
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
     }
-  slides.forEach(slides => slides.classList.remove("displayslide"));
-  slides[slideIndex].classList.add("displayslide");
+    slides.forEach(slides => slides.classList.remove("displayslide"));
+    slides[slideIndex].classList.add("displayslide");
 }
 slides[slideIndex].classList.add("displayslide");
-function prevslide(){
-
+function prevslide() {
+ clearInterval(interalId);   
+ slideIndex--;
+ showslide(slideIndex);
 }
-function nextslide(){
-   slideIndex++;
-   showslide(slideIndex);
+function nextslide() {
+    slideIndex++;
+    showslide(slideIndex);
 }
 
